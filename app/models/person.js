5a2f46f2c462e6cde8bb134151901a6648fb34df
Nishant Gada <gada.ni@northeastern.edu>
2023-11-20 18:32:51 -0500

    Initial Commit

---
+++ app/models/person.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    profession: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    food_preferences: {
+        type: Number,
+        required: true
+    },
+    pet_preferences: {
+        type: Number
+    },
+},
+{
+    versionKey: false
+});
+
+const PersonModel = mongoose.model("Person", PersonSchema);
+
+export default PropertyModel;
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,5 +2,5 @@ import propertyRouter from './property-route.js';
 
 export default (app) => {
     app.use('/property', propertyRouter);
-    
+    app.use('/getPersonDetails');
 };
