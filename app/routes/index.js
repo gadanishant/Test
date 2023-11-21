84df08943bced4e96686268e12f3fbe52e520bee
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-11-20 19:31:38 -0500

    Update the end point name

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 import propertyRouter from './property-route.js';
 
 export default (app) => {
-    app.use('/property', propertyRouter);
+    app.use('/getPropertyDetails', propertyRouter);
     
 };
