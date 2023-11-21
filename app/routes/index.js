19d95bfa1aed5a325db1bdfbefd97e28010ee1e3
Nishant Gada <gada.ni@northeastern.edu>
2023-11-21 15:41:59 -0500

    Configured Person Router in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import propertyRouter from './property-route.js';
+import personRouter from './person-route.js';
 
 export default (app) => {
     app.use('/property', propertyRouter);
-    app.use('/getPersonDetails');
+    app.use('/getPersonDetails', personRouter);
 };
