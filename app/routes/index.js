fd928b0d5f2c6883118d390dfff2d6fb3acf7d38
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-11-21 15:52:04 -0500

    index.js updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,5 +2,5 @@ import propertyRouter from './property-route.js';
 
 export default (app) => {
     app.use('/getPropertyDetails', propertyRouter);
-    
+    app.use('/getApartmentDetails', propertyRouter);
 };
