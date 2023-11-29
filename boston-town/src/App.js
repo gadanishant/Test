9850c73290ac0a7b70f2bc4a7aa3b2081e352aab
gadanishant <gada.ni@northeastern.edu>
2023-11-28 20:21:04 -0500

    Added react-router-dom to the project

--- boston-town/src/App.js
+++ boston-town/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/navbar/navbar';
+import Login from './pages/customer/login/login';
+import Signup from './pages/customer/signup/signup';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Welcome to Boston Town!
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<Router>
+				<div>
+					<Navbar />
+					<hr />
+					<Routes>
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/login" element={<Login />} />
+					</Routes>
+				</div>
+			</Router>
 		</div>
 	);
 }
