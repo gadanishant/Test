822282297b79ce55d75467b5b20153ed0077a1ca
aptekedar <apte.k@northeastern.edu>
2023-12-02 19:12:25 -0500

    AntDesign Install and Footer Image push

--- app/services/app.js
+++ app/services/app.js
@@ -8,18 +8,12 @@ const initialize = (app) => {
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded());
-
-    // MongoDB
-    mongoose.connect('mongodb+srv://KedarApte:@Kedarapte2004@webdesignfall2023.0vsqfhl.mongodb.net/incidentdb?retryWrites=true&w=majority');
     
     // Initialize routes
     registerRouter(app);
     
-<<<<<<< HEAD
-=======
     // MongoDB
     mongoose.connect('mongodb+srv://wilson:123qweasdzxc@zhe-test.vxpolmd.mongodb.net/?retryWrites=true&w=majority');
->>>>>>> 49bf10dbcedc3ff3cb4778f8b2a436f8f849a0f0
 };
 
 export default initialize;
