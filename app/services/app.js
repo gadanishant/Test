83b0ad2401834b4cddf4e4867743882ea4debc6b
gadanishant <gada.ni@northeastern.edu>
2023-12-02 19:23:29 -0500

    Added MongoDB Driver connection string

--- app/services/app.js
+++ app/services/app.js
@@ -13,7 +13,7 @@ const initialize = (app) => {
     registerRouter(app);
     
     // MongoDB
-    mongoose.connect('mongodb+srv://wilson:123qweasdzxc@zhe-test.vxpolmd.mongodb.net/?retryWrites=true&w=majority');
+    mongoose.connect('mongodb+srv://nishant:nishant@zhe-test.vxpolmd.mongodb.net/?retryWrites=true&w=majority');
 };
 
 export default initialize;
