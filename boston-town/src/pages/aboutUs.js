ad5fdec127c322fcb073ce0fc1d451ee2d1867aa
aptekedar <apte.k@northeastern.edu>
2023-12-03 18:33:27 -0500

    About us commit

---
+++ boston-town/src/pages/aboutUs.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import "./aboutUs.css"
+
+const About = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default About
