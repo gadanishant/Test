fa8190905838d7563dac97d338df6f71e38d5c20
aptekedar <apte.k@northeastern.edu>
2023-12-03 18:33:05 -0500

    Footer commit

--- boston-town/src/components/footer/footer.js
+++ boston-town/src/components/footer/footer.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import footer_image from "./boston_town_footer_image.png";
-import {Button} from 'antd';
 import "./footer.css"
+import {Button} from 'antd';
+import styles from "../../assets/css/custom_antd.css"
 
 const Footer = () => {
     return (
         <>
+        <div class = "footer">
+        <nav>   
+            <ul>
+                <li><a href = "./aboutus">About</a></li>
+                <li><a href = "./contactus">Contact Us</a></li>
+            </ul>
+        </nav>
+        </div>
         <img id = "footerImage" src = {footer_image} alt = "Boston_Town_Footer_Image"></img>
-        <Button>footer</Button>
+        <Button className = "test">test</Button>
+        <Button>test2</Button>
         </>
     );
 }
