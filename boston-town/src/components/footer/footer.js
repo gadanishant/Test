857769b1580e86429ac88334c6621b57e0ccad6b
aptekedar <apte.k@northeastern.edu>
2023-12-05 21:20:54 -0500

    Customer signup and footerImage

--- boston-town/src/components/footer/footer.js
+++ boston-town/src/components/footer/footer.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
+import myImage from '../../../src/assets/images/Boston_Town_Footer_Image.png';
 
 const Footer = () => {
     return (
         <>
-            <Button variant="contained">Footer</Button>
+        <img src = {myImage}></img>
         </>
     );
 }
