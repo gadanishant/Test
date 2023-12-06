44a7408ca4a4950e18de83d986a9bc2466fd3f5b
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-05 21:06:32 -0500

    feed.js and css created

---
+++ boston-town/src/pages/feed.js
@@ -0,0 +1,38 @@
+import { Card, Row, Col } from "antd";
+// import "./feed.css";
+// import "boston-town/src/assets/css/global_css.css";
+import ".././assets/css/global_css.css";
+
+const Feed = () => {
+    return (
+        <>
+            <Row className="background" gutter={[24,24]}>
+                <Col xs={6} sm={6} md={6} lg={5} xl={5} xxl={5}>
+                    <Card className="card">
+
+                    </Card>
+                </Col>
+                <Col xs={6} sm={6} md={6} lg={13} xl={13} xxl={13}>
+                    <Card className="card">
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col>
+                            </Col>
+
+                        </Row>
+
+                    </Card>
+                </Col>
+                <Col xs={2} sm={4} md={8} lg={6} xl={6} xxl={6}>
+                    <Card className="card">
+
+                    </Card>
+                </Col>
+
+            </Row>
+        </>
+    );
+}
+
+export default Feed;
