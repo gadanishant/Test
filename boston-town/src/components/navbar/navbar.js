77ef1bf9e22ed790c15f11c20db58eb2ca1a859c
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-05 21:06:52 -0500

    footer and nav updated

--- boston-town/src/components/navbar/navbar.js
+++ boston-town/src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 import "./navbar.css";
 
@@ -6,12 +6,28 @@ import "./navbar.css";
 const Navbar = () => {
     return (
         <>
-            <h3>Navbar!</h3>
             <nav>
-                <ul>
-                    <li><Link to="/signup">signup</Link></li>
-                    <li><Link to="/login">login</Link></li>
-                </ul>
+                <Row class gutter={[16,16]}>
+                    <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
+                       <h2> <Link to="/feed">feed</Link></h2> 
+                    </Col>
+                    <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
+                    <h2> Find a roomate</h2> 
+                       
+                    </Col>
+                    <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
+                      <h2> Contact Agent</h2> 
+                    </Col>
+                    <Col xs={2} sm={4} md={6} lg={6} xl={6} xxl={6}>
+                        <img></img>
+                    </Col>
+                    <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
+                    <h2>  <Link to="/signup">signup</Link> </h2>
+                    </Col>
+                    <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
+                    <h2> <Link to="/login">login</Link> </h2>
+                    </Col>
+                </Row>
             </nav>
         </>
     );
