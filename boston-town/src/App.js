0f1fa6512f914a1d8cc2c13e8e9177cbe941f183
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-05 21:07:15 -0500

    global css created

--- boston-town/src/App.js
+++ boston-town/src/App.js
@@ -1,8 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/footer';
 import Navbar from './components/navbar/navbar';
 import Login from './pages/customer/login/login';
 import Signup from './pages/customer/signup/signup';
+import Home from './pages/home';
+import Feed from './pages/feed';
 
 function App() {
 	return (
@@ -11,7 +13,10 @@ function App() {
 				<div>
 					<Navbar />
 					<hr />
+					{/* <Home /> */}
 					<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/feed" element={<Feed />} />
 						<Route path="/signup" element={<Signup />} />
 						<Route path="/login" element={<Login />} />
 					</Routes>
