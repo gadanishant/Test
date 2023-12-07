812c9b2a84c7a75d79cf906a4a95921d99d23d95
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-06 20:01:59 -0500

    navbar updated

--- boston-town/src/components/navbar/navbar.js
+++ boston-town/src/components/navbar/navbar.js
@@ -1,36 +1,32 @@
 import { Col, Row } from 'antd';
 import { Link } from 'react-router-dom';
-import "./navbar.css";
 import logo from '../../../src/assets/images/logo.png';
-import "../../"
+import "./navbar.css";
 
 const Navbar = () => {
     return (
-        <>
-            <nav>
-                <Row class="background"  gutter={[16, 16]}>
-                    <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
-                        <h2> <Link to="/feed">feed</Link></h2>
-                    </Col>
-                    <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
-                        <h2> Find a roomate</h2>
-
-                    </Col>
-                    <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
-                        <h2> Contact Agent</h2>
-                    </Col>
-                    <Col xs={2} sm={4} md={6} lg={6} xl={6} xxl={6}>
-                        <img src={logo} alt="" />
-                    </Col>
-                    <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
-                        <h2>  <Link to="/signup">signup</Link> </h2>
-                    </Col>
-                    <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
-                        <h2> <Link to="/login">login</Link> </h2>
-                    </Col>
-                </Row>
-            </nav>
-        </>
+        <div>
+            <Row className="background margin_auto" gutter={[16, 16]}>
+                <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
+                    <h2> <Link to="/feed">Feed</Link></h2>
+                </Col>
+                <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
+                    <h2> Find a roomate</h2>
+                </Col>
+                <Col xs={2} sm={4} md={6} lg={4} xl={4} xxl={4}>
+                    <h2> Contact Agent</h2>
+                </Col>
+                <Col xs={2} sm={4} md={6} lg={6} xl={6} xxl={6}>
+                    <img class="nav_logo" src={logo} alt="" />
+                </Col>
+                <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
+                    <h2> <Link to="/signup">signup</Link> </h2>
+                </Col>
+                <Col xs={2} sm={4} md={6} lg={3} xl={3} xxl={3}>
+                    <h2> <Link to="/login">login</Link> </h2>
+                </Col>
+            </Row>
+        </div>
     );
 }
 
