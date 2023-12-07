3f06885f8e8ed5e36a417191e9c93708a279b0f8
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-06 20:02:09 -0500

    feed page card created

--- boston-town/src/pages/feed.js
+++ boston-town/src/pages/feed.js
@@ -1,26 +1,44 @@
-import { Card, Row, Col } from "antd";
-// import ".././assets/css/global_css.css";
-// import logo from '../../public/images/logo.png'
+import { Card, Col, Row, Divider } from "antd";
+import "./feed.css";
+
+import { UserOutlined } from "@ant-design/icons"
+
+
 const Feed = () => {
     return (
         <>
-            <Row className="background" gutter={[24,24]}>
+        <div className='padding background_feed '>
+            <Row  gutter={[24, 24]}>
                 <Col xs={6} sm={6} md={6} lg={5} xl={5} xxl={5}>
-                    <Card className="card">
-
-                    </Card>
+                    <Card className="card"></Card>
                 </Col>
                 <Col xs={6} sm={6} md={6} lg={13} xl={13} xxl={13}>
                     <Card className="card">
                         <Row>
                             <Col>
-                            <img src=""></img>
+                            <UserOutlined />
                             </Col>
                             <Col>
+                            <Row>
+                            <Link to="/username">User Name</Link>
+                            </Row>
+                            <Row>
+                                New apartment
+                            </Row>
+                            <Row>
+                                5 Hours Ago
+                            </Row>
                             </Col>
-
                         </Row>
-
+                        <Row>
+                            <h3>
+                                This is a new feed post
+                            </h3>
+                        </Row>
+                        <Row>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, deleniti.
+                        </Row>
+                        <Divider></Divider>
                     </Card>
                 </Col>
                 <Col xs={2} sm={4} md={8} lg={6} xl={6} xxl={6}>
@@ -30,6 +48,7 @@ const Feed = () => {
                 </Col>
 
             </Row>
+            </div>
         </>
     );
 }
