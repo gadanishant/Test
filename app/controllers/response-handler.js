4a2e2b409fbde5dae08c74d6deb5464a5f34ab70
gadanishant <gada.ni@northeastern.edu>
2023-12-09 20:16:28 -0500

    Added additional method to handle errors

--- app/controllers/response-handler.js
+++ app/controllers/response-handler.js
@@ -2,6 +2,14 @@ export const setResponse = (data, res) => {
     res.status(200).json(data);
 }
 
+export const badRequest = (err, res) => {
+    console.log("badRequest: err => ", err);
+    res.status(400).json({
+        code: "Bad Request",
+        message: "One or more required parameters are missing!"
+    });
+}
+
 export const setErrorResponse = (err, res) => {
     console.log("err => ", err);
     res.status(500).json({
