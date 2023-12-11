3283da303a27072bebe077e5819f6ca25380f48a
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-11 14:33:38 -0500

    Create chat.js

---
+++ app/models/chat.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = Schema({
+    senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+  });
+
+const ChatModel = mongoose.model("chat", MessageSchema);
+
+export default ChatModel;
