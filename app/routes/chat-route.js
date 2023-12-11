8bac7dbe23565f13635d9367cce4ccd6810bc647
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-11 16:12:22 -0500

    Update route url for checking chat history
    Add IDs of both sender and receiver

--- app/routes/chat-route.js
+++ app/routes/chat-route.js
@@ -4,6 +4,6 @@ import * as chatController from '../controllers/chat-controller.js';
 const router = express.Router();
 
 router.post('/send', chatController.sendChatMessage);
-router.get('/history/:userId', chatController.getChatHistory);
+router.get('/history/:userId1/:userId2', chatController.getChatHistory);
 
 export default router;
