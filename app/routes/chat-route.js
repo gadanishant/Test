0ca21db1e782eca294396861293c41fc934f4f26
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-11 15:56:40 -0500

    Update chat-route.js

--- app/routes/chat-route.js
+++ app/routes/chat-route.js
@@ -3,7 +3,7 @@ import * as chatController from '../controllers/chat-controller.js';
 
 const router = express.Router();
 
-router.post('/send', chatController.sendMessage);
-router.get('/history/:userId', chatController.getMessageHistory);
+router.post('/send', chatController.sendChatMessage);
+router.get('/history/:userId', chatController.getChatHistory);
 
 export default router;
