a60f0e0ee44a8e14fe1489c50659f958b82dd9fe
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-11 16:12:48 -0500

    Optimized chat model

--- app/models/chat.js
+++ app/models/chat.js
@@ -3,10 +3,24 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const MessageSchema = Schema({
-    senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    content: String,
-    timestamp: { type: Date, default: Date.now },
+    senderId: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User',
+      required: true
+    },
+    receiverId: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User',
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    timestamp: { 
+      type: Date, 
+      default: Date.now 
+    },
   });
 
 const ChatModel = mongoose.model("Chat", MessageSchema);
