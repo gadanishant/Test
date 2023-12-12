fb0684e0677af1b67f9933b5dd6d8e13c5160b5c
gadanishant <gada.ni@northeastern.edu>
2023-12-12 12:22:37 -0500

    Added loader to Feed page

--- boston-town/src/pages/feed.js
+++ boston-town/src/pages/feed.js
@@ -2,17 +2,19 @@ import { UserOutlined } from "@ant-design/icons";
 import { Card, Col, Divider, Row } from "antd";
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import "./feed.css";
+import Loader from "../components/loader";
 import sendRequest from "../components/sendRequest";
-
+import "./feed.css";
 
 const Feed = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("isAuthenticated"));
     const [listOfPosts, setListOfPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const getAllPostsAPI = async () => {
         try {
             const response = await sendRequest("http://localhost:3000/getAllPosts", {}, "GET", {});
+            setTimeout(() => { setLoading(false) }, [1000 * 3])
             const data = response.data.description.slice(1);
             console.log("data => ", data);
             setListOfPosts(data)
@@ -27,108 +29,70 @@ const Feed = () => {
     }, [sessionStorage.getItem("isAuthenticated")])
 
     useEffect(() => {
+        setLoading(true);
         getAllPostsAPI();
     }, [])
 
     return (
         (isAuthenticated === "true"
             ? <>
-                <Col xs={6} sm={6} md={6} lg={5} xl={5} xxl={5}>
-                    <Card className="card"></Card>
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={13} xl={13} xxl={13}>
-                    <Card className="card">
-                        <Row>
-                            <Col>
-                                <UserOutlined />
-                            </Col>
-                            <Col>
-                                <Row>
-                                    <Link to="/profile">User Name</Link>
-                                </Row>
-                                <Row>
-                                    New apartment
-                                </Row>
-                                <Row>
-                                    5 Hours Ago
-                                </Row>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <h3>
-                                This is a new feed post
-                            </h3>
-                        </Row>
-                        <Row>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, deleniti.
-                        </Row>
-                        <Divider></Divider>
-                    </Card>
-                </Col>
-                <Col xs={2} sm={4} md={8} lg={6} xl={6} xxl={6}>
-                    <Card className="card">
+                {loading ? <Loader /> :
+                    <div className='padding_background_feed'>
+                        <Row gutter={[24, 24]}>
+                            {
+                                listOfPosts.map((post) => (
+                                    <div key={post.id}>
+                                        <Col xs={6} sm={6} md={6} lg={5} xl={5} xxl={5}>
+                                            <Card className="card"></Card>
+                                        </Col>
+                                        <Col xs={6} sm={6} md={6} lg={13} xl={13} xxl={13}>
+                                            <Card className="card">
+                                                <Row>
+                                                    <Col>
+                                                        <UserOutlined />
+                                                    </Col>
+                                                    <Col>
+                                                        <Row>
+                                                            <Link to="/profile">{post.username}</Link>
+                                                        </Row>
+                                                        <Row>
+                                                            {post.title}
+                                                        </Row>
+                                                        <Row>
+                                                            {post.timestamp}
+                                                        </Row>
+                                                    </Col>
+                                                </Row>
+                                                <Row>
+                                                    <h3>
+                                                        This is a new feed post
+                                                    </h3>
+                                                </Row>
+                                                <Row>
+                                                    {post.description}
+                                                </Row>
+                                                <Divider></Divider>
+                                            </Card>
+                                        </Col>
+                                        <Col xs={2} sm={4} md={8} lg={6} xl={6} xxl={6}>
+                                            <Card className="card">
 
-                    </Card>
-                </Col>
-                <div className='padding_background_feed'>
-                    <Row gutter={[24, 24]}>
-                        {
-                            listOfPosts.map((post) => (
-                                <div key={post.id}>
-                                    <Col xs={6} sm={6} md={6} lg={5} xl={5} xxl={5}>
-                                        <Card className="card"></Card>
-                                    </Col>
-                                    <Col xs={6} sm={6} md={6} lg={13} xl={13} xxl={13}>
-                                        <Card className="card">
-                                            <Row>
-                                                <Col>
-                                                    <UserOutlined />
-                                                </Col>
-                                                <Col>
-                                                    <Row>
-                                                        <Link to="/profile">{post.username}</Link>
-                                                    </Row>
-                                                    <Row>
-                                                        {post.title}
-                                                    </Row>
-                                                    <Row>
-                                                        {post.timestamp}
-                                                    </Row>
-                                                </Col>
-                                            </Row>
-                                            <Row>
-                                                <h3>
-                                                    This is a new feed post
-                                                </h3>
-                                            </Row>
-                                            <Row>
-                                                {post.description}
-                                            </Row>
-                                            <Divider></Divider>
-                                        </Card>
-                                    </Col>
-                                    <Col xs={2} sm={4} md={8} lg={6} xl={6} xxl={6}>
-                                        <Card className="card">
-
-                                        </Card>
-                                    </Col>
-                                </div>
-                            ))
-                        }
-                    </Row>
-                </div>
+                                            </Card>
+                                        </Col>
+                                    </div>
+                                ))
+                            }
+                        </Row>
+                    </div>
+                }
             </>
             : <>
                 <div className='padding_background_feed'>
                     <p>Kindly login!</p>
                 </div>
-            </>)
+            </>
+        )
     );
 }
 
 export default Feed;
-
-
-/*
-
-*/
