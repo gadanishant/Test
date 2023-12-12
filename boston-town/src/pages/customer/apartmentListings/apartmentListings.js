7e67d0ff75f93033f98d12a4e6eb75c755148eec
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-12 15:43:41 -0500

    multiple neighbou filter added

--- boston-town/src/pages/customer/apartmentListings/apartmentListings.js
+++ boston-town/src/pages/customer/apartmentListings/apartmentListings.js
@@ -9,7 +9,8 @@ const ApartmentListings = () => {
 	const [filteredProperties, setFilteredProperties] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1); // State to manage current page
 	const pageSize = 8; // Number of items per page
-	console.log(listOfProperties)
+	const [selectedNeighborhoods, setSelectedNeighborhoods] = useState([]);
+
 	const getAllpropertyAPI = async () => {
 		try {
 			const response = await sendRequest(
@@ -30,37 +31,58 @@ const ApartmentListings = () => {
 	}, []);
 
 	useEffect(() => {
-		if (zipCodeFilter.trim() === '') {
+		if (zipCodeFilter.trim() === '' && selectedNeighborhoods.length === 0) {
 			setFilteredProperties(listOfProperties);
 		} else {
-			const filtered = listOfProperties.filter(
-				(property) => property.zip_code === zipCodeFilter.trim()
-			);
+			let filtered = listOfProperties;
+
+			if (zipCodeFilter.trim() !== '') {
+				filtered = filtered.filter(
+					(property) => property.zip_code === zipCodeFilter.trim()
+				);
+			}
+
+			if (selectedNeighborhoods.length > 0) {
+				filtered = filtered.filter((property) =>
+					selectedNeighborhoods.includes(property.neighborhood)
+				);
+			}
+
 			setFilteredProperties(filtered);
 		}
 		setCurrentPage(1); // Reset to first page when filters change
-	}, [listOfProperties, zipCodeFilter]);
+	}, [listOfProperties, zipCodeFilter, selectedNeighborhoods]);
 
-	// Calculate the properties to display based on pagination
 	const indexOfLastProperty = currentPage * pageSize;
 	const indexOfFirstProperty = indexOfLastProperty - pageSize;
-	const currentProperties = filteredProperties.slice(indexOfFirstProperty, indexOfLastProperty);
+	const currentProperties = filteredProperties.slice(
+		indexOfFirstProperty,
+		indexOfLastProperty
+	);
 
 	const handlePageChange = (page) => {
 		setCurrentPage(page);
 	};
-	// Assuming 'listOfProperties' is an array of objects containing property information
 
-	// Create a Set to store unique neighborhoods
 	const uniqueNeighborhoods = new Set();
-
-	// Filtering out duplicates and populating the uniqueNeighborhoods Set
 	listOfProperties.forEach((apartment) => {
 		uniqueNeighborhoods.add(apartment.neighborhood);
 	});
-
-	// Converting Set back to an array
 	const uniqueNeighborhoodsArray = Array.from(uniqueNeighborhoods);
+
+	const handleNeighborhoodSelect = (neighborhood) => {
+		const updatedSelection = [...selectedNeighborhoods];
+
+		if (updatedSelection.includes(neighborhood)) {
+			const index = updatedSelection.indexOf(neighborhood);
+			updatedSelection.splice(index, 1);
+		} else {
+			updatedSelection.push(neighborhood);
+		}
+
+		setSelectedNeighborhoods(updatedSelection);
+	};
+
 	return (
 		<>
 			<div className="paddingListings">
@@ -71,18 +93,27 @@ const ApartmentListings = () => {
 							<Divider />
 							<div>
 								Zipcode
+								<br/>
 								<Input
 									value={zipCodeFilter}
 									onChange={(e) => setZipCodeFilter(e.target.value)}
 								/>
 							</div>
+							<br/>
+							<br/>
+							<br/>
 							<div>
 								Neighborhood
+								<br/>
 								<Row gutter={16}>
 									{uniqueNeighborhoodsArray.map((neighborhood, index) => (
 										<Col span={24} key={index}>
-											<div className='neighborhood_card'>
-											{neighborhood}
+											<div
+												className={`neighborhood_card ${selectedNeighborhoods.includes(neighborhood) ? 'selected' : ''
+													}`}
+												onClick={() => handleNeighborhoodSelect(neighborhood)}
+											>
+												{neighborhood}
 											</div>
 										</Col>
 									))}
@@ -117,13 +148,13 @@ const ApartmentListings = () => {
 								</Col>
 							))}
 						</Row>
-						{/* <Pagination
+						<Pagination
 							current={currentPage}
 							total={filteredProperties.length}
 							pageSize={pageSize}
 							onChange={handlePageChange}
 							style={{ marginTop: '20px', textAlign: 'center' }}
-						/> */}
+						/>
 					</Col>
 				</Row>
 			</div>
