a285ed50ff8f0eaa0d94b59fd788cdfd3d456175
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2023-12-13 17:53:03 -0500

    Carousel added

--- boston-town/src/pages/customer/apartmentListings/apartmentdetails.js
+++ boston-town/src/pages/customer/apartmentListings/apartmentdetails.js
@@ -1,23 +1,34 @@
-
 import './apartmentdetails.css';
 import React from 'react';
-import { Carousel, Row, Col } from 'antd';
+import { Carousel, Row, Col, Button } from 'antd'; // Import Button from antd
+import logo from '../../../../src/assets/images/logo4.png';
+
 const contentStyle = {
     height: '160px',
     color: '#fff',
     lineHeight: '160px',
     textAlign: 'center',
     background: '#364d79',
-  };
+};
 
 const ApartmentDetails = () => {
+    const carouselRef = React.useRef(); // Create a reference to the carousel component
+
+    const next = () => {
+        carouselRef.current.next(); // Function to go to the next slide
+    };
+
+    const prev = () => {
+        carouselRef.current.prev(); // Function to go to the previous slide
+    };
+
     return (
         <div className="details_padding">
             <Row>
                 <Col span={16}>
-                    <Carousel autoplay>
+                    <Carousel autoplay ref={carouselRef}>
                         <div>
-                            <h3 style={contentStyle}>1</h3>
+                            <h3 style={contentStyle}> <img src={logo} alt="logo" /></h3>
                         </div>
                         <div>
                             <h3 style={contentStyle}>2</h3>
@@ -29,10 +40,16 @@ const ApartmentDetails = () => {
                             <h3 style={contentStyle}>4</h3>
                         </div>
                     </Carousel>
+                    <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                        {/* Button to go to previous slide */}
+                        <Button onClick={prev} style={{ marginRight: '10px' }}>Previous</Button>
+                        {/* Button to go to next slide */}
+                        <Button onClick={next}>Next</Button>
+                    </div>
                 </Col>
             </Row>
         </div>
     );
-}
+};
 
 export default ApartmentDetails;
