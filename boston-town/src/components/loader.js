4ce81688cebaa6121cdf4d311da2066d9da1f35d
gadanishant <gada.ni@northeastern.edu>
2023-12-13 14:51:09 -0500

    Modified the loader

--- boston-town/src/components/loader.js
+++ boston-town/src/components/loader.js
@@ -3,6 +3,7 @@ import "./loader.css"
 const Loader = () => {
     return (
         <section>
+            {/* 
             <svg
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 300 150'>
@@ -22,6 +23,48 @@ const Loader = () => {
                         repeatCount='indefinite'>
                     </animate>
                 </path>
+            </svg>
+             */}
+            <svg
+                id="loader-svg"
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 200 200'>
+                <circle
+                    fill='none'
+                    stroke-opacity='1'
+                    stroke='#FF156D'
+                    stroke-width='.5'
+                    cx='100'
+                    cy='100'
+                    r='0'>
+                    <animate
+                        attributeName='r'
+                        calcMode='spline'
+                        dur='2'
+                        values='1;80'
+                        keyTimes='0;1'
+                        keySplines='0 .2 .5 1'
+                        repeatCount='indefinite'>
+                    </animate>
+                    <animate
+                        attributeName='stroke-width'
+                        calcMode='spline'
+                        dur='2'
+                        values='0;25'
+                        keyTimes='0;1'
+                        keySplines='0 .2 .5 1'
+                        repeatCount='indefinite'>
+                    </animate>
+                    <animate
+                        attributeName='stroke-opacity'
+                        calcMode='spline'
+                        dur='2'
+                        values='1;0'
+                        keyTimes='0;1'
+                        keySplines='0 .2 .5 1'
+                        repeatCount='indefinite'>
+                    </animate>
+                </circle>
             </svg>
         </section>
     )
